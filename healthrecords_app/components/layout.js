cb7694c5a4ee8b565d32bcd8f93dea41036d7016
Derek-Jianwei Xiao <derek-jianwei.xiao@tuhh.de>
2024-05-17 12:31:21 +0200

    Added Layout and CSS skeleton

---
+++ healthrecords_app/components/layout.js
@@ -0,0 +1,9 @@
+export default function App(){
+    return(
+        <>
+        <p>Header</p>
+
+        <p>Footer</p>
+        </>
+    )
+}
---
+++ healthrecords_app/pages/Provider.js
@@ -0,0 +1,8 @@
+
+import React from 'react'
+
+export default function Provider() {
+  return (
+    <div>Provider</div>
+  )
+}
