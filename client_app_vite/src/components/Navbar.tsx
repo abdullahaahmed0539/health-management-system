20a3edfccca63e8cb40b6b82c153eeb747ef6c73
Derek-Jianwei Xiao <derek-jianwei.xiao@tuhh.de>
2024-06-13 00:22:12 +0200

    Logout Button

--- client_app_vite/src/components/Navbar.tsx
+++ client_app_vite/src/components/Navbar.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../context/useAuth";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
+  const { isLoggedIn, user, logoutUser } = useAuth();
+
   return (
     <NavbarBs className="bg-white shadow-sm mb-3">
       <Container>
@@ -15,12 +18,22 @@ const Navbar = (props: Props) => {
           <Nav.Link to="/search" as={NavLink}>
             Search
           </Nav.Link>
-          <Nav.Link to="login" as={NavLink}>
-            Login
-          </Nav.Link>
-          <Nav.Link to="/signup" as={NavLink}>
-            SignUp
-          </Nav.Link>
+        </Nav>
+        <Nav className="ms-auto">
+          {isLoggedIn() ? (
+            <Button onClick={logoutUser} className="ms-2">
+              Logout
+            </Button>
+          ) : (
+            <>
+              <Nav.Link to="/login" as={NavLink} className="ms-2">
+                Login
+              </Nav.Link>
+              <Nav.Link to="/signup" as={NavLink} className="ms-2">
+                SignUp
+              </Nav.Link>
+            </>
+          )}
         </Nav>
       </Container>
     </NavbarBs>
