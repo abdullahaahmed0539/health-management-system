b7542a937b7efda64ac252172f43e4feb3afea62
abdullahaahmed0539 <abdullahaahmed0539@gmail.com>
2024-06-14 00:36:59 +0200

    Added treatment creation endpoint

--- server_app/lib/controllers/treatment-controller.ts
+++ server_app/lib/controllers/treatment-controller.ts
@@ -80,8 +80,60 @@ class TreatmentController implements Controller {
   }
 
   async add(req: Request, res: Response): Promise<Response> {
-    throw new Error("Method not implemented.");
+    try {
+      const token = req.headers.authorization!.split(" ")[1];
+      const userId = req.params.patientId;
+      const verifiedUser = jwt.verify(token, process.env.JWT_PVT_KEY as string);
+      const role = (<any>verifiedUser).role;
+      if (role === "doctor" || role === "sysAdmin") {
+        const { name, diseaseName, doctorId, doctorName, treatmentDate } =
+          req.body;
+
+        if (
+          !name ||
+          !diseaseName ||
+          !doctorId ||
+          !doctorName ||
+          !treatmentDate
+        ) {
+          return res.status(400).json({
+            error: {
+              message:
+                "Please Enter name, disease name, doctor id, doctor name, treatment date",
+            },
+          });
+        }
+
+        const patient = await Patient.findOne({ userId });
+        if (!patient)
+          return res.status(404).json({
+            error: {
+              message: `No patient with user id ${userId} found.`,
+            },
+          });
+        let newTreatment = new Treatment();
+        newTreatment.name = name;
+        newTreatment.diseaseName;
+        newTreatment.doctorId;
+        newTreatment.doctorName = doctorName;
+        newTreatment.treatmentDate = treatmentDate;
+
+        patient.treatmentHistory.push(newTreatment);
+        await patient.updateOne(patient);
+        return res.status(201).json({
+          patient,
+        });
+      }
+      return res
+        .status(401)
+        .json({ error: { message: "Unauthorized Access." } });
+    } catch (err: any) {
+      const errorMessage: string = (err as Error).message;
+      logger.error(errorMessage);
+      return res.status(500).json({ errorMessage });
+    }
   }
+    
   update(req: Request, res: Response): Promise<Response> {
     throw new Error("Method not implemented.");
   }
