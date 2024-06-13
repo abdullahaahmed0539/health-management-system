1a88f3b3e57d7035c67e74b54144b6b0f80fcb1b
abdullahaahmed0539 <abdullahaahmed0539@gmail.com>
2024-06-14 00:36:21 +0200

    Added treatment get by id endpoint

--- server_app/lib/controllers/treatment-controller.ts
+++ server_app/lib/controllers/treatment-controller.ts
@@ -8,49 +8,77 @@ import Treatment from "../models/treatment";
 
 
 class TreatmentController implements Controller {
-    async getAll(req: Request, res: Response): Promise<Response> {
-         try {
-           const token = req.headers.authorization!.split(" ")[1];
-           const userId = req.params.patientId;
-           const verifiedUser = jwt.verify(
-             token,
-             process.env.JWT_PVT_KEY as string
-           );
-           const role = (<any>verifiedUser).role;
-           const idFromToken = (
-             await User.findOne({ email: (<any>verifiedUser).email as string })
-           )?.id;
+  async getAll(req: Request, res: Response): Promise<Response> {
+    try {
+      const token = req.headers.authorization!.split(" ")[1];
+      const userId = req.params.patientId;
+      const verifiedUser = jwt.verify(token, process.env.JWT_PVT_KEY as string);
+      const role = (<any>verifiedUser).role;
+      const idFromToken = (
+        await User.findOne({ email: (<any>verifiedUser).email as string })
+      )?.id;
 
-           if (
-             role === "sysAdmin" ||
-             role === "doctor" ||
-             userId === idFromToken
-           ) {
-             const patient = await Patient.findOne({ userId });
-             if (!patient)
-               return res.status(404).json({
-                 error: {
-                   message: `No patient with userId ${userId} found.`,
-                 },
-               });
-             return res.status(200).json(patient.treatmentHistory);
-           }
-           return res.status(401).json({
-             error: {
-               message: `Unauthorized access.`,
-             },
-           });
-         } catch (err: any) {
-           const errorMessage: string = (err as Error).message;
-           logger.error(errorMessage);
-           return res.status(500).json({ errorMessage });
-         }
+      if (role === "sysAdmin" || role === "doctor" || userId === idFromToken) {
+        const patient = await Patient.findOne({ userId });
+        if (!patient)
+          return res.status(404).json({
+            error: {
+              message: `No patient with userId ${userId} found.`,
+            },
+          });
+        return res.status(200).json(patient.treatmentHistory);
+      }
+      return res.status(401).json({
+        error: {
+          message: `Unauthorized access.`,
+        },
+      });
+    } catch (err: any) {
+      const errorMessage: string = (err as Error).message;
+      logger.error(errorMessage);
+      return res.status(500).json({ errorMessage });
     }
+  }
 
   async get(req: Request, res: Response): Promise<Response> {
-    throw new Error("Method not implemented.");
+    try {
+      const token = req.headers.authorization!.split(" ")[1];
+      const userId = req.params.patientId;
+      const treatmentId = req.params.treatmentId;
+      const verifiedUser = jwt.verify(token, process.env.JWT_PVT_KEY as string);
+      const role = (<any>verifiedUser).role;
+      const idFromToken = (
+        await User.findOne({ email: (<any>verifiedUser).email as string })
+      )?.id;
+
+      if (role === "sysAdmin" || role === "doctor" || userId === idFromToken) {
+        const patient = await Patient.findOne({ userId });
+        if (!patient)
+          return res.status(404).json({
+            error: {
+              message: `No patient with userId ${userId} found.`,
+            },
+          });
+        return res
+          .status(200)
+          .json(
+            patient.treatmentHistory.find(
+              (x) => x._id.toString() === treatmentId
+            )
+          );
+      }
+      return res.status(401).json({
+        error: {
+          message: `Unauthorized access.`,
+        },
+      });
+    } catch (err: any) {
+      const errorMessage: string = (err as Error).message;
+      logger.error(errorMessage);
+      return res.status(500).json({ errorMessage });
+    }
   }
-    
+
   async add(req: Request, res: Response): Promise<Response> {
     throw new Error("Method not implemented.");
   }
