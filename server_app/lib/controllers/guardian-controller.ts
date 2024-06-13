c144b4f2e57d2e0222db7f583f86f2eee383e12a
abdullahaahmed0539 <abdullahaahmed0539@gmail.com>
2024-06-14 01:33:59 +0200

    Added guardian create endpoint

--- server_app/lib/controllers/guardian-controller.ts
+++ server_app/lib/controllers/guardian-controller.ts
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 import { Controller } from "./interfaces/controller";
 import Patient from "../models/patient";
 import User from "../models/user";
+import Guardian from "../models/guardian-contact";
 
 class GuardianController implements Controller {
   async getAll(req: Request, res: Response): Promise<Response> {
@@ -44,7 +45,50 @@ class GuardianController implements Controller {
 
   async add(req: Request, res: Response): Promise<Response> {
     try {
-      throw new Error("Method not implemented.");
+      const token = req.headers.authorization!.split(" ")[1];
+      const userId = req.params.patientId;
+      const verifiedUser = jwt.verify(token, process.env.JWT_PVT_KEY as string);
+      const role = (<any>verifiedUser).role;
+      if (role === "doctor" || role === "sysAdmin") {
+        const { name, email, phone, relation } =
+          req.body;
+
+        if (
+          !name ||
+          !email ||
+          !phone ||
+          !relation 
+        ) {
+          return res.status(400).json({
+            error: {
+              message:
+                "Please Enter name, email, phone & relation.",
+            },
+          });
+        }
+
+        const patient = await Patient.findOne({ userId });
+        if (!patient)
+          return res.status(404).json({
+            error: {
+              message: `No patient with user id ${userId} found.`,
+            },
+          });
+        let newGuardian = new Guardian();
+        newGuardian.name = name;
+        newGuardian.email = email;
+        newGuardian.phone = phone;
+        newGuardian.relation = relation;
+
+        patient.guardianInfo.push(newGuardian);
+        await patient.updateOne(patient);
+        return res.status(201).json({
+          guardian: newGuardian,
+        });
+      }
+      return res
+        .status(401)
+        .json({ error: { message: "Unauthorized Access." } });
     } catch (err: any) {
       const errorMessage: string = (err as Error).message;
       logger.error(errorMessage);
