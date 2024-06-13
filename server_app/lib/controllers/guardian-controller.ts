d20bafdb12773733d6f1fd3c761a0386b4551d52
abdullahaahmed0539 <abdullahaahmed0539@gmail.com>
2024-06-14 01:37:09 +0200

    Added guardian get by id endpoint

--- server_app/lib/controllers/guardian-controller.ts
+++ server_app/lib/controllers/guardian-controller.ts
@@ -39,8 +39,41 @@ class GuardianController implements Controller {
     }
   }
 
-  get(req: Request, res: Response): Promise<Response> {
-    throw new Error("Method not implemented.");
+  async get(req: Request, res: Response): Promise<Response> {
+    try {
+      const token = req.headers.authorization!.split(" ")[1];
+      const userId = req.params.patientId;
+      const guardianId = req.params.guardianId;
+      const verifiedUser = jwt.verify(token, process.env.JWT_PVT_KEY as string);
+      const role = (<any>verifiedUser).role;
+      const idFromToken = (
+        await User.findOne({ email: (<any>verifiedUser).email as string })
+      )?.id;
+
+      if (role === "sysAdmin" || role === "doctor" || userId === idFromToken) {
+        const patient = await Patient.findOne({ userId });
+        if (!patient)
+          return res.status(404).json({
+            error: {
+              message: `No patient with userId ${userId} found.`,
+            },
+          });
+        return res.status(200).json({
+          treatment: patient.guardianInfo.find(
+            (x) => x._id.toString() === guardianId
+          ),
+        });
+      }
+      return res.status(401).json({
+        error: {
+          message: `Unauthorized access.`,
+        },
+      });
+    } catch (err: any) {
+      const errorMessage: string = (err as Error).message;
+      logger.error(errorMessage);
+      return res.status(500).json({ errorMessage });
+    }
   }
 
   async add(req: Request, res: Response): Promise<Response> {
