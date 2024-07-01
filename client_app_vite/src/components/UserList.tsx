f5f9f5af9a6d5479222ec9e5d43d8f33c1c896ca
Ilayda Güner <ilayda.guener@nithh.de>
2024-07-01 16:07:49 +0200

    Update file UserList.tsx

--- client_app_vite/src/components/UserList.tsx
+++ client_app_vite/src/components/UserList.tsx
@@ -1,49 +1,46 @@
-import { useEffect, useState } from "react";
-import { RawProfileData } from "../models/User";
-import { Table } from "react-bootstrap";
-import { fetchUsers } from "../services/UserService";
+import React from 'react';
+import { Table, Alert } from "react-bootstrap";
 
-interface Props {}
+interface User {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+}
 
-const UserList = (props: Props) => {
-  const [users, setUsers] = useState<RawProfileData[]>();
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+interface Props {
+  users: User[];
+}
 
-  useEffect(() => {
-    const getUserList = async () => {
-      const result = await fetchUsers();
-      setUsers(result);
-    };
-    getUserList();
-    // console.log(users);
-  }, []);
+const UserList = ({ users }: Props) => {
+    if (!users || users.length === 0) {
+        return <Alert variant="info">No users</Alert>;
+    }
 
-  return (
-    <>
-      <h1>User List</h1>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            {/* Add other headers as needed */}
-          </tr>
-        </thead>
-        <tbody>
-          {users &&
-            users.map((user, index) => (
-              <tr key={index}>
-                <td>{user._id}</td>
-                <td>{user.firstName.concat(" ", user.lastName)}</td>
-                <td>{user.email}</td>
-              </tr>
-            ))}
-        </tbody>
-      </Table>
-    </>
-  );
+    return (
+        <>
+            <h1>User List</h1>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map(user => (
+                        <tr key={user._id}>
+                            <td>{user._id}</td>
+                            <td>{user.firstName} {user.lastName}</td>
+                            <td>{user.email}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </>
+    );
 };
 
 export default UserList;
+
